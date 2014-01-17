d0e94478c26c8019f5cdb3d1afcd1ae7c8092f1f
Syed Sayem <syed@hotmail.com>
2014-01-16 20:54:37 -0500

    Added ex4, and ex5

--- LearnPythonHardWay/ex5.py
+++ LearnPythonHardWay/ex5.py
@@ -1,18 +1,19 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35
+height = 56
+height_in_cm = 2.56
+weight = 180
+weight_in_kg = weight * 0.45359237
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
-print "Actually that's not too heavy"
-print "He's got %s eyes and %s hair"% (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffe." % my_teeth
+print "Let's talk about %r." % name
+print "He's %r inches tall (%r cm)." % (height, height_in_cm)
+print "Actually that's not too heavy."
+print "He's got %r eyes and %r hair." % (eyes, hair)
+print "His teeth are usually %r depending on the coffe." % teeth
 
-# this line is tricky, try to get it exatly right
-print "If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age +
-	my_height + my_weight)
+# this line is tricky, try to to get it exactly right
+print "If I add %r, %r, and %r I get %r." % (
+    age, height, weight, age + height + weight)
